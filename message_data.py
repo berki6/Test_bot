73cc15f48c6f87b2f05ff521ffce42db541c098c
berki6 <bereketfikadu71@gmail.com>
2024-10-21 23:39:39 +0300

    fixed message_data on the scheduling problem on the time zone- fix 2

--- message_data.py
+++ message_data.py
@@ -14,11 +14,18 @@ class ReminderData:
         return "Message:  {0}; At Time:  {1}".format(self.message, self.time.strftime('%d/%m/%Y %H:%M'))
 
     def should_be_fired(self):
-        gmt_plus_3 = pytz.timezone('Etc/GMT-3')
-        now = datetime.datetime.now(gmt_plus_3)
+            gmt_plus_3 = pytz.timezone('Etc/GMT-3')
+            now = datetime.datetime.now(gmt_plus_3)
 
-        if self.time.tzinfo is None:
-            self.time = gmt_plus_3.localize(self.time)
+            utc = pytz.UTC
+            if self.time.tzinfo is None:
+                self.time = utc.localize(self.time)
 
-        return self.fired is False and now >= self.time
+            reminder_time_gmt_plus_3 = self.time.astimezone(gmt_plus_3)
+
+            print(f"Now (GMT+3): {now}")
+            print(f"Reminder time (GMT+3): {reminder_time_gmt_plus_3}")
+
+            return self.fired is False and now >= reminder_time_gmt_plus_3
+
 

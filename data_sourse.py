ce5f3672bc1e78867ca8dec40ec9a13caed97449
berki6 <bereketfikadu71@gmail.com>
2024-10-21 11:05:57 +0300

    db update

--- data_sourse.py
+++ data_sourse.py
@@ -15,7 +15,7 @@ class DataSource:
         self.database_url = database_url
 
     def get_connection(self):
-        return psycopg2.connect(self.database_url, sslmode= 'allow')
+        return psycopg2.connect(self.database_url, sslmode= 'require')
 
     @staticmethod
     def close_connection(conn):
